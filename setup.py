c25946f59ff884af457ea56f8fa942b8a0af004f
Walter A. Alves <walter.arruda.alves@gmail.com>
2018-05-20 17:47:06 -0300

    Add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name = "twitter_storm",
+    version = "0.0.1",
+    description = ("Loggi twitter storm hiring challenge"),
+    packages=find_packages()
+)
