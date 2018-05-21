80b93422dc135a361e05ba3b2ce32aeebfd25007
Walter A. Alves <walter.arruda.alves@gmail.com>
2018-05-21 02:23:06 -0300

    add all files generated by running sudo python setup.py install --user

---
+++ build/lib/twitter_storm/main.py
@@ -0,0 +1,26 @@
+from core.tweet_broker import TweetBroker
+import sys, os
+
+def main():
+    if len(sys.argv) != 2:
+        print("You should run main by running the following command:\n\n\33[32mpython \33[33mmain.py \33[0m\33[44m\"The text you would like to send\"" )
+        exit(1)
+    no_variables = False
+    if not os.environ.has_key('TWITTER_CONSUMER_KEY'):
+        print("You should define TWITTER_CONSUMER_KEY environment variable")
+        no_variables = True
+    if not os.environ.has_key('TWITTER_CONSUMER_SECRET'):
+        print("You should define TWITTER_CONSUMER_SECRET environment variable")
+        no_variables = True
+
+    if no_variables:
+        exit(1)
+
+    tweet_broker = TweetBroker(os.environ['TWITTER_CONSUMER_KEY'], os.environ['TWITTER_CONSUMER_SECRET'], sys.argv[1])
+    tweet_broker.do_auth()
+
+    tweet_broker.prepare()
+
+    tweet_broker.start()
+if __name__ == "__main__":
+    main()
---
+++ build/lib/twitter_storm/utils/get_tokens.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+
+from requests_oauthlib import OAuth1Session
+import webbrowser
+import os
+import sys
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+SIGNIN_URL = 'https://api.twitter.com/oauth/authenticate'
+
+
+def get_access_token(consumer_key, consumer_secret):
+    oauth_client = OAuth1Session(consumer_key, client_secret=consumer_secret, callback_uri='oob')
+
+    print('\nRequesting token from Twitter...\n')
+
+    try:
+        resp = oauth_client.fetch_request_token(REQUEST_TOKEN_URL)
+    except ValueError as e:
+        raise 'Invalid response from Twitter requesting temp token: {0}'.format(e)
+
+    url = oauth_client.authorization_url(AUTHORIZATION_URL)
+
+    print('A browser will try to visit the following Twitter page '
+          'if a browser will not start, copy the URL to your browser '
+          'and retrieve the pincode to be used '
+          'in the next step to obtaining an Authentication Token: \n'
+          '\n\t{0}'.format(url))
+
+    webbrowser.open(url)
+    pincode = raw_input('\nEnter your pincode? ')
+
+    print('\nGenerating and signing request for an access token...\n')
+
+    oauth_client = OAuth1Session(consumer_key, client_secret=consumer_secret,
+                                 resource_owner_key=resp.get('oauth_token'),
+                                 resource_owner_secret=resp.get('oauth_token_secret'),
+                                 verifier=pincode)
+    try:
+        resp = oauth_client.fetch_access_token(ACCESS_TOKEN_URL)
+    except ValueError as e:
+        raise 'Invalid response from Twitter requesting temp token: {0}'.format(e)
+
+    auth_token = resp.get('oauth_token')
+    auth_secret = resp.get('oauth_token_secret')
+
+    print("Done!!")
+
+    return {'consumer_key':consumer_key, 'consumer_secret':consumer_secret,  'token_key': auth_token, 'token_secret': auth_secret }
